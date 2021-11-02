fb837817919bef578ed9592a762c5818fc9c8c43
salim-ouari <salim.ouari@laplateforme.io>
2021-11-02 11:39:46 +0100

    commit2

---
+++ jour01/job01/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+
+echo "$str2 $str $str3";
+
+$val = "6";
+echo "$val";
+$val = "\n 4";
+echo "$val";
+
+$mybool = "true";
+echo " $mybool";
+$mybool = "\n false";
+echo "$mybool";
