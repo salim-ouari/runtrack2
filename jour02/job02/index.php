20fb3c53e801866ea93c4c249ee5000c7f4c5933
salim-ouari <salim.ouari@laplateforme.io>
2021-11-03 16:37:27 +0100

    découverte while

---
+++ jour02/job02/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$var=0;  /*Initialisation de la variable de boucle  */
+
+	while /*tant que */ ($var<=1337)
+	{
+		if /*si*/ ($var==26 ||$var==37 || $var==88 ||$var==1111 ||$var==3233)
+		
+		 	echo NULL.'<br>';
+		
+		else /*sinon*/
+		{
+			echo $var."<br/>";
+		}
+		
+		$var++; /*$var prend 1*/
+	}
+
+    ?>
+
+
