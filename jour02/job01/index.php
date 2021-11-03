278628c44f6a6b1f43db8665f6fbc1f78430c37c
salim-ouari <salim.ouari@laplateforme.io>
2021-11-03 11:30:52 +0100

    initiation boucles

---
+++ jour02/job01/index.php
@@ -0,0 +1,22 @@
+<?php
+
+for /* pour */  ($i = 0; $i <= 1337; $i++)
+{ 
+
+/* si */
+if  ($i ==42  )
+
+/*affiche*/
+
+{echo " <b><u> 42 </b></u> <br>";}
+
+
+
+ /*  sinon   */
+else 
+echo " $i <br>  "; 
+
+}
+
+
+
