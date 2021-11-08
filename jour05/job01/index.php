0e7e47bd66943168a5428f42ba35bb64f4163dc0
salim-ouari <salim.ouari@laplateforme.io>
2021-11-08 10:46:11 +0100

    1er function

---
+++ jour05/job01/index.php
@@ -0,0 +1,12 @@
+<!-- Créez une fonction nommée “hello”. Cette fonction doit afficher “Hello
+LaPlateforme!”. -->
+
+<?php
+$var= 'Hello LaPlateforme';
+function hello ($param){
+
+// j'echo mon paramétre
+echo $param;
+}
+// appeller ma function
+hello($var);
